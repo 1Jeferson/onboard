cd8d7fc2ba687480bfeb2e15d2f5bd9691bf8e02
1Jeferson <jefersonnanet.95@gmail.com>
2025-02-17 10:59:49 -0300

    feat: refactor login page import structure and create index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from './app/modules/auth/login.page';
+import { LoginPage } from './app/modules/auth/';
 import authRoutes from './app/modules/auth/auth.routes';
 
 function App() {
