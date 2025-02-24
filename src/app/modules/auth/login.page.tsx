bbfa574f21943ec28eed752db98f5a1a9f04196c
1Jeferson <jefersonnanet.95@gmail.com>
2025-02-24 14:25:46 -0300

    feat:Implemented the TextInput component with style and icon support

--- src/app/modules/auth/login.page.tsx
+++ src/app/modules/auth/login.page.tsx
@@ -1,25 +1,11 @@
-import { InputCaption, InputLabel, InputValue, Link, Text } from '@/atomic/atm.typography';
+import { TextInput } from '@/atomic/atm.text-input';
 
 const LoginPage = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-2 p-small'>
-      <Text variant='display'>Display</Text>
-      <Text variant='h1'>Heading 1</Text>
-      <Text variant='h2'>Heading 2</Text>
-      <Text variant='h3'>Heading 3</Text>
-      <Text variant='h4'>Heading 4</Text>
-      <Link variant='linkSmall' href='/'>
-        Link small
-      </Link>
-      <Link href='/auth/login' variant='link'>
-        Link
-      </Link>
-      <Text variant='b1'>Body</Text>
-      <Text variant='b2'>Body 2</Text>
-
-      <InputCaption>Input caption</InputCaption>
-      <InputLabel>Input Label</InputLabel>
-      <InputValue>Input Value</InputValue>
+    <div className='flex justify-center items-center gap-2 p-small'>
+      <TextInput variant='default' label='Input label' icon='info' placeholder='Default' caption='Input caption' />
+      <TextInput variant='error' label='Input label' placeholder='Error' icon='info' caption='Input caption' />
+      <TextInput variant='default' disabled label='Input label' placeholder='Disabled' />
     </div>
   );
 };
---
+++ src/atomic/atm.text-input/text-input.component.tsx
@@ -0,0 +1,47 @@
+import { ReactNode } from 'react';
+import { InputCaption, InputLabel } from '../atm.typography';
+import { textInputStye, TextInputVariants } from './text-input.component.style';
+import { twMerge } from 'tailwind-merge';
+import { Info } from '../assets/icons';
+
+interface TextInputProps extends TextInputVariants {
+  className?: string;
+  placeholder?: string;
+  label?: ReactNode;
+  caption?: ReactNode;
+  disabled?: boolean;
+  icon?: 'info';
+  iconPosition?: 'left' | 'right';
+}
+
+const iconMap = {
+  info: Info,
+};
+
+const TextInput = ({
+  placeholder,
+  variant,
+  className,
+  label,
+  caption,
+  disabled,
+  icon,
+  iconPosition = 'right',
+}: TextInputProps) => {
+  const IconComponent = icon ? iconMap[icon] : null;
+
+  return (
+    <div className={twMerge('flex flex-col', className)}>
+      <div
+        className={`flex items-center ${iconPosition === 'right' ? 'flex-row' : 'flex-row-reverse'} gap-1 min-h-[20px]`}
+      >
+        {label && <InputLabel>{label}</InputLabel>}
+        {IconComponent && <IconComponent />}
+      </div>
+      <input className={textInputStye({ variant, disabled })} placeholder={placeholder} disabled={disabled} />
+      <div className='min-h-[20px]'>{caption && <InputCaption>{caption}</InputCaption>}</div>
+    </div>
+  );
+};
+
+export default TextInput;
