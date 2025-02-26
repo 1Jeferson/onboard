337d55b420ccb5b2628aab5858043055747e59a6
1Jeferson <jefersonnanet.95@gmail.com>
2025-02-26 14:11:18 -0300

    refactor: Updated SVG icon sizes and attributes for visual consistency and accessibility

--- src/atomic/assets/icons/arrow.svg.tsx
+++ src/atomic/assets/icons/arrow.svg.tsx
@@ -1,13 +1,14 @@
 const Arrow = () => {
   return (
-    <svg width='20' height='20' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
+      <title>Voltar</title>
       <path
         d='M9.57 5.92969L3.5 11.9997L9.57 18.0697M20.5 11.9997H3.67'
         stroke='#0B6C54'
-        stroke-width='1.5'
-        stroke-miterlimit='10'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.5'
+        strokeMiterlimit='10'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
