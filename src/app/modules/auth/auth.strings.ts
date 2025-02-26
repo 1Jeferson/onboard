1c2fdd121c72431d15cd46df669c9e1c01522d80
1Jeferson <jefersonnanet.95@gmail.com>
2025-02-26 14:12:05 -0300

    feat: Added authentication strings for labels and placeholders

---
+++ src/app/modules/auth/auth.strings.ts
@@ -0,0 +1,10 @@
+export const authStrings = {
+  emailLabel: 'E-mail',
+  emailPlaceholder: 'Digite seu e-mail',
+  passwordLabel: 'Senha',
+  passwordPlaceholder: 'Digite sua senha',
+  Button: {
+    login: 'Login',
+    register: 'Registrar',
+  },
+};
--- src/app/modules/auth/login.page.tsx
+++ src/app/modules/auth/login.page.tsx
@@ -1,12 +1,23 @@
 import { Button } from '@/atomic/atm.button';
 import { TextInput } from '@/atomic/atm.text-input';
+import { authStrings } from './auth.strings';
 
 const LoginPage = () => {
   return (
     <div className='flex w-lg m-auto flex-col items-center gap-2x-small p-small'>
-      <TextInput variant='default' type='email' placeholder='Digite seu e-mail' label='E-mail' />
-      <TextInput placeholder='Digite sua senha' type='password' variant='default' label='Senha' />
-      <Button variant='primary'>Login</Button>
+      <TextInput
+        variant='default'
+        type='email'
+        placeholder={authStrings.emailPlaceholder}
+        label={authStrings.emailLabel}
+      />
+      <TextInput
+        placeholder={authStrings.passwordPlaceholder}
+        type='password'
+        variant='default'
+        label={authStrings.passwordLabel}
+      />
+      <Button variant='primary'>{authStrings.Button.login}</Button>
     </div>
   );
 };
