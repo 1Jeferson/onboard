7179d73f982ea7236500f261d7a412c3cab5f3be
1Jeferson <jefersonnanet.95@gmail.com>
2025-02-27 10:41:02 -0300

    feat: implement login form with validation using Zod and React Hook Form with shadcn form

--- src/app/modules/auth/login.page.tsx
+++ src/app/modules/auth/login.page.tsx
@@ -1,24 +1,69 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { Form, FormField, FormItem, FormControl } from '@/components/ui/form';
 import { Button } from '@/atomic/atm.button';
 import { TextInput } from '@/atomic/atm.text-input';
 import { authStrings } from './auth.strings';
+import { LoginSchema, loginSchema } from './auth.schema';
 
 const LoginPage = () => {
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: LoginSchema) => {
+    console.log('Form Data:', data);
+  };
+
   return (
-    <div className='flex w-lg m-auto flex-col items-center gap-2x-small p-small'>
-      <TextInput
-        variant='default'
-        type='email'
-        placeholder={authStrings.emailPlaceholder}
-        label={authStrings.emailLabel}
-      />
-      <TextInput
-        placeholder={authStrings.passwordPlaceholder}
-        type='password'
-        variant='default'
-        label={authStrings.passwordLabel}
-      />
-      <Button variant='primary'>{authStrings.button.login}</Button>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col gap-x-small max-w-md m-auto'>
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormControl>
+                <TextInput
+                  label={authStrings.emailLabel}
+                  placeholder={authStrings.emailPlaceholder}
+                  variant={fieldState.error ? 'error' : 'default'}
+                  type='email'
+                  {...field}
+                  caption={fieldState.error?.message}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormControl>
+                <TextInput
+                  label={authStrings.passwordLabel}
+                  placeholder={authStrings.passwordPlaceholder}
+                  variant={fieldState.error ? 'error' : 'default'}
+                  type='password'
+                  {...field}
+                  caption={fieldState.error?.message}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit' variant='primary' children={authStrings.button.login} />
+      </form>
+    </Form>
   );
 };
 
---
+++ src/components/ui/form.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import {
+  Controller,
+  ControllerProps,
+  FieldPath,
+  FieldValues,
+  FormProvider,
+  useFormContext,
+  useFormState,
+} from 'react-hook-form';
+
+import { cn } from '@/lib/utils';
+
+const Form = FormProvider;
+
+type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  name: TName;
+};
+
+const FormFieldContext = React.createContext<FormFieldContextValue>({} as FormFieldContextValue);
+
+const FormField = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  ...props
+}: ControllerProps<TFieldValues, TName>) => {
+  return (
+    <FormFieldContext.Provider value={{ name: props.name }}>
+      <Controller {...props} />
+    </FormFieldContext.Provider>
+  );
+};
+
+const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext);
+  const itemContext = React.useContext(FormItemContext);
+  const { getFieldState } = useFormContext();
+  const formState = useFormState({ name: fieldContext.name });
+  const fieldState = getFieldState(fieldContext.name, formState);
+
+  if (!fieldContext) {
+    throw new Error('useFormField should be used within <FormField>');
+  }
+
+  const { id } = itemContext;
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  };
+};
+
+type FormItemContextValue = {
+  id: string;
+};
+
+const FormItemContext = React.createContext<FormItemContextValue>({} as FormItemContextValue);
+
+function FormItem({ className, ...props }: React.ComponentProps<'div'>) {
+  const id = React.useId();
+
+  return (
+    <FormItemContext.Provider value={{ id }}>
+      <div data-slot='form-item' className={cn('grid gap-2x-small', className)} {...props} />
+    </FormItemContext.Provider>
+  );
+}
+
+function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
+  const { error, formItemId, formDescriptionId, formMessageId } = useFormField();
+
+  return (
+    <Slot
+      data-slot='form-control'
+      id={formItemId}
+      aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`}
+      aria-invalid={!!error}
+      {...props}
+    />
+  );
+}
+
+export { useFormField, Form, FormItem, FormControl, FormField };
