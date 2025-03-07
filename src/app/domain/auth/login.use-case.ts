382b53969ea0ce51d630e842a6f8b089d29772af
1Jeferson <jefersonnanet.95@gmail.com>
2025-03-07 11:14:48 -0300

    feat:integrated user stores and authentication in the login use case

--- src/app/domain/auth/login.use-case.ts
+++ src/app/domain/auth/login.use-case.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@apollo/client';
 import { LoginMutation, LoginMutationVariables, LoginDocument } from '@/app/data/graphql/generated';
+import { useAuthStore, useUserStore } from '@/app/store';
 
 interface LoginUseCase {
   onCompleted?: (data?: LoginMutation) => void;
@@ -7,14 +8,27 @@ interface LoginUseCase {
 }
 
 export const useLogin = ({ onCompleted, onError }: LoginUseCase) => {
+  const { setUser } = useUserStore();
+  const { setToken } = useAuthStore();
+
   const [loginMutation, { loading }] = useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, {
-    onCompleted: (response) => onCompleted?.(response),
+    onCompleted: (response) => {
+      if (response?.login) {
+        const { token, user } = response.login;
+
+        setUser({ id: user.id, name: user.name });
+        setToken(token);
+      }
+      onCompleted?.(response);
+    },
     onError: (err) => onError?.(err),
   });
+
   const login = (variables: LoginMutationVariables) => {
     if (loading) return;
 
     loginMutation({ variables });
   };
+
   return { login, loading };
 };
