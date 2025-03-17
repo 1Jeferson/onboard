b9012fac158e3bad04e87e8e09f91e67c0903d10
1Jeferson <jefersonnanet.95@gmail.com>
2025-03-17 13:22:55 -0300

    refactor: removed fixed width for logo and avatar images in HomeLayout

--- src/app/modules/home/homeLayout.tsx
+++ src/app/modules/home/homeLayout.tsx
@@ -8,12 +8,12 @@ const HomeLayout = () => {
   return (
     <div className='flex flex-col h-screen items-center'>
       <nav className='w-full max-w-2xlarge flex items-center justify-between gap-medium p-3x-small sm:py-2x-small sm:px-2x-large'>
-        <img src='/logo.png' alt='Logo Instaq' className='w-16 h-16 sm:w-large sm:h-large' />
+        <img src='/logo.png' alt='Logo Instaq' className='sm:w-large sm:h-large' />
 
         <div>
           <LinkButton variant='link' icon='pickerDown' iconPosition='right' className='flex gap-2x-small items-center'>
             {name}
-            <img src='/Perfil2.png' alt='Avatar' className='w-10 sm:w-medium' />
+            <img src='/Perfil2.png' alt='Avatar' className='sm:w-medium' />
           </LinkButton>
         </div>
       </nav>
