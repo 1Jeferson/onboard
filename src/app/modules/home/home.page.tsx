ac5140ac20bbd07fac7d9148154527dfbcc8191c
1Jeferson <jefersonnanet.95@gmail.com>
2025-03-17 16:58:34 -0300

    feat:added localization strings and EmptyProject component to HomePage

--- src/app/modules/home/home.page.tsx
+++ src/app/modules/home/home.page.tsx
@@ -1,5 +1,15 @@
+import { Text } from '@/atomic/atm.typography';
+import { homeStrings } from './home.strings';
+import { EmptyProject } from '@/atomic/atm.empty-projetc';
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Text variant='h1'>{homeStrings.allProjects}</Text>
+
+      <EmptyProject />
+    </div>
+  );
 };
 
 export default HomePage;
--- src/app/modules/home/index.ts
+++ src/app/modules/home/index.ts
@@ -1,5 +1,5 @@
 import HomePage from './home.page';
 import kanbanRoutes from './kanban.routes';
-import HomeLayout from './HomeLayout';
+import HomeLayout from './homeLayout';
 
 export { HomePage, kanbanRoutes, HomeLayout };
