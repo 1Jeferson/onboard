edd0290c8a8a85323dc7b62e66aeada544765da8
1Jeferson <jefersonnanet.95@gmail.com>
2025-03-21 13:20:17 -0300

    feat: added edit functionality to BoardCard component and updated strings for project editing

--- src/atomic/atm.board-card/board-card.component.tsx
+++ src/atomic/atm.board-card/board-card.component.tsx
@@ -1,6 +1,9 @@
-import { kanbanRoutes } from '@/app/modules/home';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { kanbanRoutes } from '@/app/modules/home';
 import { Text } from '../atm.typography';
+import { Edit } from '../assets/icons';
+import { BoardUpdate } from '../atm.board-update';
 
 interface BoardCardProps {
   id: string;
@@ -9,18 +12,36 @@ interface BoardCardProps {
 }
 
 const BoardCard = ({ id, name, imageUrl }: BoardCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate(kanbanRoutes.board.replace(':boardId', id));
   };
 
+  const handleEditClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsModalOpen(true);
+  };
+
   return (
-    <div className='cursor-pointer rounded-large p-x-small flex flex-col gap-2x-small' onClick={handleClick}>
-      <img src={imageUrl || './CardDefault.png'} alt={name} className='w-full h-3x-large object-cover rounded-medium' />
-      <Text variant='h3' className='px-x-small'>
-        {name}
-      </Text>
+    <div className='rounded-large p-x-small flex flex-col gap-2x-small' onClick={handleClick}>
+      <img
+        src={imageUrl || './CardDefault.png'}
+        alt={name}
+        className='w-full h-3x-large object-cover rounded-medium cursor-pointer'
+      />
+      <div className='flex items-center justify-between'>
+        <Text variant='h3' className='px-x-small'>
+          {name}
+        </Text>
+
+        <button className='cursor-pointer' onClick={handleEditClick}>
+          <Edit />
+        </button>
+      </div>
+
+      <BoardUpdate id={id} name={name} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   );
 };
