f50154c0a3927cb9a3ee3356a507c8484af563e7
1Jeferson <jefersonnanet.95@gmail.com>
2025-03-24 11:52:36 -0300

    feat: componente Toaster personalizado com cores ricas e botão de fechar

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,19 +1,7 @@
 import { Toaster as Sonner, ToasterProps } from 'sonner';
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  return (
-    <Sonner
-      className='toaster group'
-      {...props}
-      toastOptions={{
-        classNames: {
-          toast: '!border-none',
-          success: '!text-gray-light !bg-feedback-success-medium',
-          error: '!text-gray-light !bg-feedback-error-medium',
-        },
-      }}
-    />
-  );
+  return <Sonner className='toaster group' {...props} />;
 };
 
 export { Toaster };
