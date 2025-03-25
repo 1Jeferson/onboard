36b75c853dbb189063bda6689fe825ef5e0cb247
1Jeferson <jefersonnanet.95@gmail.com>
2025-03-25 19:54:02 -0300

    feat: added board query and corresponding use case for fetching board details

---
+++ src/app/domain/boards/board.use-case.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@apollo/client';
+import { BoardDocument, BoardQuery, BoardQueryVariables } from '@/app/data/graphql/generated';
+
+export const useBoard = ({
+  onCompleted,
+  onError,
+  variables,
+}: {
+  variables: BoardQueryVariables;
+  onCompleted?: (data: BoardQuery) => void;
+  onError?: (error: Error) => void;
+}) => {
+  const { data, loading } = useQuery<BoardQuery, BoardQueryVariables>(BoardDocument, {
+    variables,
+    onCompleted,
+    onError,
+  });
+
+  return { data, loading };
+};
